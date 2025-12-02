f192fe21a7c72ca3c958b328bc20a27cdbf6571b
Mitnick10 <73492944+Mitnick10@users.noreply.github.com>
2025-12-02 08:00:51 -0400

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,10 +1,1 @@
-@echo off
-cd /d "%~dp0"
-set FLASK_APP=project/app.py
-set FLASK_ENV=development
-set FLASK_PORT=5000
-set FLASK_HOST=127.0.0.1
-echo Iniciando aplicacion Flask en localhost:5000...
-echo Accede a: http://localhost:5000
-python project/app.py
-pause
+
